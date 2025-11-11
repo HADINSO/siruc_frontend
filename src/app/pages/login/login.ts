8adc5e2af9d794d27bfa7c869b3791b3fc22ea10
Stivinsonweb <correamaturanaestivinson@gmail.com>
2025-11-11 12:35:23 -0500

    Agregar zona.js y mejorar la configuración de inicio de sesión

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  usuario: string = '';
+  contrasena: string = '';
 
+  constructor(private authService: Auth) {}
+
+  onLogin() {
+    this.authService.login(this.usuario, this.contrasena).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+      },
+      error: (error) => {
+        console.error('Error en login:', error);
+      }
+    });
+  }
 }
